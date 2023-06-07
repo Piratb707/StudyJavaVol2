e0b305f3ce294a05b82ced2f4a9b56bd3fefb5c9
Your Name <beloshapkobm@yandex.ru>
2023-06-07 14:05:40 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+/*
+    Класс main
+*/
+public class Main {
+    public static void main(String[] args) {
+        /*
+        Объявление переменных
+        num_a - первая переменная
+        num_b - вторая переменная
+        sum - сумма чисел
+         */
+        int num_a = 5;
+        int num_b = 8;
+        int sum = num_a + num_b;
+
+        // Вывод на печать sum
+        System.out.println(sum);
+    }
+}
